17e9c4a3ff73349d4e39916d53bf24104800b109
Ajin-001 <155342574+Ajin-001@users.noreply.github.com>
2024-10-23 08:52:52 +0530

    Add files via upload

--- logout.php
+++ logout.php
@@ -1,7 +1,15 @@
 <?php
-   session_start();
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+/**
+ * Logout script
+ *
+ * @package PhpMyAdmin
+ */
+require_once 'libraries/common.inc.php';
 
-   if(session_destroy()) {
-      header("Location: login.php");
-   }
-?>
+if ($token_mismatch) {
+    PMA_sendHeaderLocation('./index.php');
+} else {
+    $auth_plugin->logOut();
+}
+
