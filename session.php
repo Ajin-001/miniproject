f0ca834af745d9323bdaab3d5b45173475c4304d
Ajin-001 <155342574+Ajin-001@users.noreply.github.com>
2024-10-23 08:48:44 +0530

    Add files via upload

--- session.php
+++ session.php
@@ -1,21 +1,21 @@
 <?php
-$link = mysqli_connect("localhost", "root", "","phpmyadmin");
+$link = mysqli_connect("localhost", "root", "","logintest");
  if($link === false)
 {
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
    session_start();
-/*
+
    $user_check = $_SESSION['login_user'];
 
-   $ses_sql = mysqli_query($link,"select id from pma__users where id = '$user_check' ");
+   $ses_sql = mysqli_query($link,"select uid from user where uid = '$user_check' ");
 
    $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
 
-   $login_session = $row['id'];
+   $login_session = $row['uid'];
 
    if(!isset($_SESSION['login_user']))
    {
       header("location:login.php");
-   }*/
+   }
 ?>
